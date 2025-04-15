b536874c092d8f3849a91e95018f1409b0aec638
Supertiger <saawalkhn@googlemail.com>
2025-04-15 14:59:23 +0100

    initial commit

---
+++ src/DiscordClient.ts
@@ -0,0 +1,97 @@
+import { Client, Events, GatewayIntentBits, Presence } from "discord.js";
+import {
+  GatewayDispatchPayload,
+  GatewayDispatchEvents,
+} from "discord-api-types/v9";
+
+interface FormattedPresence {
+  status: string;
+  activities: FormattedActivity[];
+}
+interface FormattedActivity {
+  name: string;
+  createdTimestamp: number | null;
+  details: string | null;
+  state: string | null;
+  syncId: string | null;
+  url?: string | null;
+  assets?: {
+    largeText?: string | null;
+    smallText?: string | null;
+    largeImage?: string | null;
+    smallImage?: string | null;
+  };
+  timestamps?: {
+    start: Date | null;
+    end: Date | null;
+  } | null;
+}
+
+const cleanEmpty = <T extends Record<string, any>>(obj: T) =>
+  Object.entries(obj)
+    .map(([k, v]) => [k, v && typeof v === "object" ? cleanEmpty(v) : v])
+    .reduce((a, [k, v]) => (v == null ? a : ((a[k] = v), a)), {});
+
+export const createDiscordClient = (token: string, guildId: string) => {
+  const events = {
+    presenceUpdate: (userId: string, presence: FormattedPresence) => {},
+    ready: () => {},
+  };
+
+  const formatPresence = (presence?: any) => {
+    if (!presence) return;
+    if (typeof presence !== "object") return;
+    return cleanEmpty({
+      status: presence.status,
+      activities: presence.activities.map(
+        (a) =>
+          ({
+            name: a.name,
+            timestamps: a.timestamps,
+            url: a.url,
+            details: a.details,
+            syncId: a.syncId,
+            state: a.state,
+            assets: a.assets,
+            createdTimestamp: a.createdTimestamp,
+          } as FormattedActivity)
+      ),
+    }) as FormattedPresence;
+  };
+
+  const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildPresences],
+  });
+
+  client.once(Events.ClientReady, (readyClient) => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    events.ready();
+  });
+
+  client.on(Events.Raw, (data) => {
+    const payload = data as GatewayDispatchPayload;
+    if (payload.t === GatewayDispatchEvents.PresenceUpdate) {
+      process.nextTick(() => {
+        events.presenceUpdate(
+          payload.d.user.id,
+          getUserPresence(payload.d.user.id) as FormattedPresence
+        );
+      });
+    }
+  });
+
+  client.login(token);
+
+  const getUserPresence = (userId: string) => {
+    const member = client.guilds.cache.get(guildId)?.members.cache.get(userId);
+    if (!member) return "MEMBER_NOT_IN_GUILD";
+    return formatPresence(
+      member?.presence?.toJSON() || { status: "offline", activities: [] }
+    );
+  };
+
+  return {
+    events,
+    getUserPresence,
+  };
+};
---
+++ src/index.ts
@@ -0,0 +1,59 @@
+import { Elysia } from "elysia";
+import { DISCORD_TOKEN, GUILD_ID, PORT } from "./env";
+import { createDiscordClient } from "./DiscordClient";
+import { rateLimit } from "elysia-rate-limit";
+
+const app = new Elysia().use(
+  rateLimit({
+    errorResponse: new Response(
+      JSON.stringify({
+        error: "Rate limit exceeded",
+        code: "RATE_LIMIT_EXCEEDED",
+      }),
+      { status: 429, headers: { "Content-Type": "application/json" } }
+    ),
+  })
+);
+
+const discordClient = createDiscordClient(DISCORD_TOKEN!, GUILD_ID!);
+
+discordClient.events.presenceUpdate = (userId, data) => {
+  app.server?.publish(userId, JSON.stringify(data));
+};
+
+const MemberNotInGuildErrorMessage =
+  "This member is not in the Discord RPC Tracker guild. Join https://discord.gg/ggrd2wr4pe and try again.";
+
+app.ws("/users/:userId/presences", {
+  open: (ws) => {
+    const userId = ws.data.params.userId;
+    const presence = discordClient.getUserPresence(userId);
+    if (presence === "MEMBER_NOT_IN_GUILD") {
+      ws.send({ error: MemberNotInGuildErrorMessage, code: presence });
+      ws.close();
+    }
+    ws.subscribe(userId);
+
+    ws.send(presence);
+  },
+});
+
+app.get("/users/:userId/presences", async ({ params, set }) => {
+  const userId = params.userId;
+
+  const presence = discordClient.getUserPresence(userId);
+  if (presence === "MEMBER_NOT_IN_GUILD") {
+    set.status = 404;
+    return {
+      error: MemberNotInGuildErrorMessage,
+    };
+  }
+
+  return presence;
+});
+
+discordClient.events.ready = () => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+};
